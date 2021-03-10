7360774202dbc3d10f48affc1365095ba917babc
zjli-2019 <zhengji.li@ingchips.com>
2021-03-10 11:03:51 +0800

    release 5.4.3:
    * Bundles:
        * [FIX] powersaving issue
    
    * Library:
        * [NEW] heap overriding option: LL heap
        * [FIX] `ota_service` module now supports powersaving

--- src/Tools/malloc_override.c
+++ src/Tools/malloc_override.c
@@ -1,9 +1,21 @@
+#include <string.h>
+
+#if (HEAP_OVERRIDE_TYPE == 0)
+
 #include "FreeRTOSConfig.h"
 #include "FreeRTOS.h"
-#include <string.h>
 
 #define _override_malloc    pvPortMalloc
 #define _override_free      vPortFree
+
+#elif (HEAP_OVERRIDE_TYPE == 1)
+
+#include "ll_api.h"
+
+#define _override_malloc    ll_malloc
+#define _override_free      ll_free
+
+#endif
 
 void * malloc(size_t s)
 {
@@ -33,3 +45,4 @@ void * realloc(void * p, size_t s)
     if (p) _override_free(p);
     return mem;
 }
+
