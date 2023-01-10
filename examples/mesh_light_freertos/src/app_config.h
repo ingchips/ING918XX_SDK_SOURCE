8d0d0356271814a29c2558f19ae3398cc2e68d91
lzk <zhk_lee@qq.com>
2023-01-10 13:17:54 +0800

    Update app_config.h

--- examples/mesh_light_freertos/src/app_config.h
+++ examples/mesh_light_freertos/src/app_config.h
@@ -1,17 +1,17 @@
 #ifndef __APP_CONFIG_H
 #define __APP_CONFIG_H
 
-// BUTTON TEST
+/* BUTTON TEST */
 #define ENABLE_BUTTON_TEST
 
-// RF test
+/* RF test */
 // #define ENABLE_RF_TX_RX_TEST
 
-// Mesh App Config
+/* Mesh App Config */
 #define ENABLE_MESH_ADV_BEARER
 #define ENABLE_MESH_GATT_BEARER
 
-// Mesh Flash Config
+/* Mesh Flash Config */
 #define USE_MESH_FLASH
 
 #endif // __APP_CONFIG_H
