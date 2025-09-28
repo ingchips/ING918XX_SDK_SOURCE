18e8ab1f9cb9e0603917f112966556b44ac189d5
zjli2019 <51557614+zjli-2019@users.noreply.github.com>
2025-09-28 14:20:00 +0800

    update ing20 bundles: fix power saving & timers

--- examples/platform_companion/scripts/make_bundle.py
+++ examples/platform_companion/scripts/make_bundle.py
@@ -11,7 +11,7 @@ PROJ_NAME = 'platform_companion'
 STACK_BIN = f'{OUTPUT_DIR}/platform.bin'
 map_fn = f'../listing/{PROJ_NAME}.map'
 
-added_api = ['platform_rom_hotfix']
+added_api = ['platform_rom_hotfix', 'platform_rom_hotfix_using_fpb']
 
 PAGE = 1024 * 4 # size of a sector
 BIN_INFO_OFFSET = 0x000000CC
