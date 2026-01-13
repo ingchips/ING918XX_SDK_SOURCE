7b211e9fb1fe7c56f6be3d1ffe62642f6285e980
zyx <yuxizhangyuxi@gmail.com>
2026-01-13 15:38:20 +0800

    add doc index.md

--- examples/eatt/doc/index.md
+++ examples/eatt/doc/index.md
@@ -2,22 +2,29 @@
 
 ## Overview
 
-The EATT (Enhanced Attribute Protocol) example demonstrates a comprehensive set of Bluetooth Low Energy (BLE) functionalities controlled via UART commands. This project show cases advanced BLE features including advertising, scanning, connection management, GATT operations over eatt channels
+This example demonstrates how to use EATT (Enhanced Attribute Protocol) to perform GATT operations over multiple ATT bearers,
 
 ## Key Features
-- **GATT Operations**: Read, write, and subscribe to characteristics over EATT channel
+- **GATT Operations**: 
+- GATT Client Operations over EATT: 
+- Service and characteristic discovery
+- Characteristic read
 
 ## Command Interface
 
-The example provides a UART-based command line interface for controlling BLE operations. Commands cover advertising, scanning, connection establishment, GATT client operations, connection parameters, power management, and subrating.
+The example provides a UART-based command line interface for controlling BLE operations. Commands cover advertising, connection establishment, EATT client operations
 
 ## Supported Platforms
 
-This example is designed for ING918XX series development boards and demonstrates the capabilities of the ING918XX BLE stack.
+This example is designed for all series development boards and demonstrates the capabilities of ING916XX ING918XX ING20.
 
 ## Getting Started
 
-1. Build and flash the example to your development boards, one is master, the other is slave
-2. slave is advertising 
-3. master board use commands like `sconn xx:xx:xx:xx:xx:xx` to to establish connection.
-4. it will connect then bonding then  perform EATT operations
+1. Build and flash the example firmware onto two development boards. One board acts as the Master, and the other acts as the Slave.
+2. After booting, the Slave starts advertising.
+3. On the Master board, use the following command to initiate a connection to the Slave:`sconn xx xx xx xx xx xx`
+4. Once the connection is established, the devices complete pairing and bonding, and EATT (Enhanced ATT) channels are successfully created.
+5. On the Master, perform GATT service and characteristic discovery using commands such as `discover`
+6. The Master reads characteristics value using a command like `read 3 6 `,3 is attribute handle, 6 is the second one.
+7. At this point, you can observe that many characteristics read are executed successfully.
+8. If use only gatt without eatt , the second characteristic read will return error .
--- examples/eatt/src/eatt_uart_console.c
+++ examples/eatt/src/eatt_uart_console.c
@@ -9,8 +9,7 @@
 #include "platform_api.h"
 #include "bluetooth.h"
 #include "sm.h"
-#include "FreeRTOS.h"
-#include "task.h"
+
 //#include "btstack_mt.h"
 
 #define GEN_OS          ((const gen_os_driver_t *)platform_get_gen_os_driver())
@@ -36,75 +35,21 @@ static void tx_data(const char *d, const uint16_t len);
 static int parse_handle_value(int *handle, const char *param);
 static const char help[] =  "commands:\n"
                             "h/?                                 show this\n"
-                            "reboot                              reboot\n"
-                            "ver                                 version\n"
-
                             "sconn  xx:xx:xx:xx:xx:xx            connect to dev using synced API\n"
-                            "read   value_handle                 read value of a characteristic\n"
-                            "sread  value_handle                 read value of a characteristic using sync. API\n"
-                            "write  handle XX XX ...             write value to a characteristic\n"
-                            "w/or   handle XX XX ...             write without response to a char.\n"
-                            "sub    handle                       subscribe to a characteristic\n"
-                            "sub/o  handle                       subscribe to a characteristic without callback\n"
-                            "unsub  handle                       unsubscribe\n"
+                            "discover                            start discovery\n"
+                            "read_several_char   value_handle1  value_handle2 ...              read several characteristics\n"
                             ;
-block_value_t periodic_test_data;
-int periodic_test_handle = 3; // 
-extern void notify_value_of_char(int handle, block_value_t *value);
-static void notify_task_entry(void *pdata)
-{
-    if (parse_handle_value(&periodic_test_handle, pdata) != 0)
-    {
-        tx_data(error, strlen(error) + 1);
-        return;
-    }
-    
-    periodic_test_data.len = sizeof(periodic_test_data.value);
-    uint8_t i = 0;
-    for(i = 0; i < periodic_test_data.len; i++)
-    {
-        periodic_test_data.value[i] = i;
-    }
-    int j = 0;
-    for (;;)
-    {
-        j++;
-        vTaskDelay(500);
-        platform_printf("periodic notify %d times\n", j);
-        notify_value_of_char(periodic_test_handle, &periodic_test_data);
-
-    }
-}
-void cmd_periodic_notify_start(const char *param)
-{
-
-    GEN_OS->task_create("notify task",
-        notify_task_entry,
-        (void *)param,
-        1024,
-        GEN_TASK_PRIORITY_LOW);
-}
 
 void cmd_help(const char *param)
 {
     tx_data(help, strlen(help) + 1);
 }
-void cmd_reboot(const char *param)
-{
-    platform_reset();
-}
+
 extern uint8_t slave_addr[];
 extern bd_addr_type_t slave_addr_type;
 
 void sync_conn_to_slave(void);
 void read_value_of_char(int handle);
-void sync_read_value_of_char(int handle);
-void write_value_of_char(int handle, block_value_t *value);
-void wor_value_of_char(int handle, block_value_t *value);
-void sub_to_char(int handle, uint8_t reg_cb);
-void unsub_to_char(int handle);
-void set_mtu(uint16_t mtu);
-
 int parse_addr(uint8_t *output, const char *param)
 {
     unsigned int addr[6];
@@ -120,157 +65,43 @@ int parse_addr(uint8_t *output, const char *param)
     return 0;
 }
 
-
 void cmd_sconn(const char *param)
 {
     if (0 == parse_addr(slave_addr, param))
         sync_conn_to_slave();
 }
-
-
-void cmd_read_char(const char *param)
+void cmd_read_several_char(const char *param)
 {
-    int t = 0;
-    if (sscanf(param, "%d", &t) != 1)
-    {
-        tx_data(error, strlen(error) + 1);
-        return;
-    }
-    read_value_of_char(t);
-}
+    const char *p = param;
+    const char *end = param + strlen(param);
+    int handle = 0;
+    while (p < end) {
 
-void cmd_sread_char(const char *param)
-{
-    int t = 0;
-    if (sscanf(param, "%d", &t) != 1)
-    {
-        tx_data(error, strlen(error) + 1);
-        return;
-    }
-    sync_read_value_of_char(t);
-}
+        while (p < end && *p == ' ') {
+            p++;
+        }
 
-block_value_t char_value;
-
-static int parse_handle_value(int *handle, const char *param)
-{
-    const char *c = param;
-    char *t;
-    while (*c == ' ') c++;
-    char_value.len = 0;
-    strcpy(buffer, c);
-    t = strtok(buffer, " ");
-    if (sscanf(t, "%d", handle) != 1)
-        return 1;
-    while ((t = strtok(NULL, " ")) != NULL)
-    {
-        if (char_value.len >= sizeof(char_value.value)) break;
-        if (sscanf(t, "%x", (int *)(char_value.value + char_value.len)) != 1)
+        if (p >= end || *p == '\n') {
             break;
-        char_value.len++;
+        }
+
+        if (sscanf(p, "%d", &handle) == 1) {
+            platform_printf("read char handle: 0x%d\n", handle);
+            read_value_of_char(handle);
+        }
+
+        while (p < end && *p != ' ' && *p != '\n') {
+            p++;
+        }
     }
-    return 0;
 }
 
-void cmd_notify_char(const char *param)
-{
-    int handle;
-
-    if (parse_handle_value(&handle, param) != 0)
-    {
-        tx_data(error, strlen(error) + 1);
-        return;
-    }
-    notify_value_of_char(handle, &char_value);
-}
-void cmd_indicate_char(const char *param)
-{
-    int handle;
-
-    if (parse_handle_value(&handle, param) != 0)
-    {
-        tx_data(error, strlen(error) + 1);
-        return;
-    }
-    extern void indicate_value_of_char(int handle, block_value_t *value);
-    indicate_value_of_char(handle, &char_value);
-}
-void cmd_write_char(const char *param)
-{
-    int handle;
-
-    if (parse_handle_value(&handle, param) != 0)
-    {
-        tx_data(error, strlen(error) + 1);
-        return;
-    }
-    write_value_of_char(handle, &char_value);
-}
-
-void cmd_wor_char(const char *param)
-{
-    int handle;
-
-    if (parse_handle_value(&handle, param) != 0)
-    {
-        tx_data(error, strlen(error) + 1);
-        return;
-    }
-    wor_value_of_char(handle, &char_value);
-}
-
-void cmd_sub_char(const char *param)
-{
-    int t = 0;
-    if (sscanf(param, "%d", &t) != 1)
-    {
-        tx_data(error, strlen(error) + 1);
-        return;
-    }
-    sub_to_char(t, 1);
-}
-
-void cmd_sub_char_without_cb(const char *param)
-{
-    int t = 0;
-    if (sscanf(param, "%d", &t) != 1)
-    {
-        tx_data(error, strlen(error) + 1);
-        return;
-    }
-    sub_to_char(t, 0);
-}
-void cmd_unsub_char(const char *param)
-{
-    int t = 0;
-    if (sscanf(param, "%d", &t) != 1)
-    {
-        tx_data(error, strlen(error) + 1);
-        return;
-    }
-    unsub_to_char(t);
-}
-
-
-void change_conn_param(int interval, int latency, int timeout);
-void ble_re_connect(void);
-void ble_show_status(void);
-
-
-static void cmd_reconn(const char *param)
-{
-    ble_re_connect();
-}
-
-static void cmd_status(const char *param)
-{
-    ble_show_status();
-}
 static void cmd_discover(const char *param)
 {
     extern void discover_services(void);
     discover_services();
 }
+
 static cmd_t cmds[] =
 {
     {
@@ -282,52 +113,12 @@ static cmd_t cmds[] =
         .handler = cmd_help
     },
     {
-        .cmd = "reboot",
-        .handler = cmd_reboot
-    },
-    {
         .cmd = "sconn",
         .handler = cmd_sconn
     },
     {
-        .cmd = "read",
-        .handler = cmd_read_char
-    },
-    {
-        .cmd = "sread",
-        .handler = cmd_sread_char
-    },
-    {
-        .cmd = "indicate",
-        .handler = cmd_indicate_char
-    },
-    {
-        .cmd = "notify",
-        .handler = cmd_notify_char
-    },
-    {
-        .cmd = "periodic_notify",
-        .handler = cmd_periodic_notify_start
-    },
-    {
-        .cmd = "write",
-        .handler = cmd_write_char
-    },
-    {
-        .cmd = "w/or",
-        .handler = cmd_wor_char
-    },
-    {
-        .cmd = "sub",
-        .handler = cmd_sub_char
-    },
-    {
-        .cmd = "sub/o",
-        .handler = cmd_sub_char_without_cb
-    },
-    {
-        .cmd = "unsub",
-        .handler = cmd_unsub_char
+        .cmd = "read_several_char",
+        .handler = cmd_read_several_char
     },
     {
         .cmd = "discover",
@@ -377,14 +168,6 @@ typedef struct
 
 str_buf_t input = {0};
 str_buf_t output = {0};
-
-/*
-void handle_command()
-{
-    tx_data("response: ", 10);
-    tx_data(input.buf, input.size);
-}
-*/
 
 static void append_data(str_buf_t *buf, const char *d, const uint16_t len)
 {
@@ -451,8 +234,6 @@ void console_rx_data(const char *d, uint8_t len)
         GEN_OS->event_set(cmd_event);
     }
 }
-
-extern void stack_notify_tx_data(void);
 
 static void tx_data(const char *d, const uint16_t len)
 {
