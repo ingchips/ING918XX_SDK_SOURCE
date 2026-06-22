1bff3eb24647181acc001f4918d130fbe863af15
yuxizhangingchips <161669215+yuxizhangingchips@users.noreply.github.com>
2026-06-22 09:30:32 +0800

    [feature]:update eatt and ead demo (#352)
    * [feature]:update eatt and ead demo
    
    * ead change scan configs
    
    * eatt reduce phy_configs

--- examples/ead/src/main.c
+++ examples/ead/src/main.c
@@ -7,6 +7,7 @@
 #include "task.h"
 #include "trace.h"
 #include "../data/setup_soc.cgen"
+
 static uint32_t cb_hard_fault(hard_fault_info_t *info, void *_)
 {
     platform_printf("HARDFAULT:\nPC : 0x%08X\nLR : 0x%08X\nPSR: 0x%08X\n"
@@ -120,24 +121,19 @@ static const platform_evt_cb_table_t evt_cb_table =
         },
     }
 };
+
 uintptr_t app_main()
 {
-#ifdef PLATFORM_IN_ROM
-    platform_rom_hotfix();
-#endif
-
     SYSCTRL_Init();
     cube_soc_init();
 
     // setup event handlers
     platform_set_evt_callback_table(&evt_cb_table);
+
     setup_peripherals();
     xTaskCreate(watchdog_task, "w", configMINIMAL_STACK_SIZE, NULL, 1, NULL);
 
     trace_rtt_init(&trace_ctx);
-    // TODO: config trace mask
-    platform_printf("eatt example build @ %s \n", __TIME__);
-    platform_config(PLATFORM_CFG_TRACE_MASK, 0x1ff);
 
     return 0;
 }
