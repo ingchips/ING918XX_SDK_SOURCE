c4122bc8c184ec74b347e6a53c1568c7ac41a4e4
zenghuiJIA <zenghui.JIA@ingchips.com>
2026-08-12 17:01:51 +0800

    fix clang-format

--- src/BSP/eflash.c
+++ src/BSP/eflash.c
@@ -377,11 +377,13 @@ static const uint8_t prog_security_page_read[] = {
 
 
 #if ((defined __ARMCC_VERSION) && (__ARMCC_VERSION < 6000000))
+// clang-format off
 asm static uint32_t security_page_read(uint32_t addr, uint32_t prog)
 {
-    ADD r1, r1, #1 
+    ADD r1, r1, #1
     BX r1
 }
+// clang-format on
 #else
 __attribute__((naked)) static uint32_t security_page_read(uint32_t addr, uint32_t prog)
 {
